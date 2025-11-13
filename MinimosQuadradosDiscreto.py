64a52c0b0e68f07b8d50440dc2d42ed41c5331a5
Gabriel Spíndola <gabrielhspindola@gmail.com>
2025-11-13 12:13:41 -0300

    Add files via upload

---
+++ MinimosQuadradosDiscreto.py
@@ -0,0 +1,34 @@
+import numpy as np
+from sympy import *
+
+x = Symbol('x')
+
+fi = [x**0, x, x**2]
+xi = [-1, 0, 1, 2]
+yi = [0, -1, 0, 7]
+
+a = np.zeros((len(fi), len(fi)))
+b = np.zeros(len(fi))
+
+def prodInt(i, j, c):
+    sum = 0
+    if c == 'a':
+        for k in xi:
+            sum += fi[i].subs(x, k)*fi[j].subs(x, k)
+    if c == 'b':
+        for k in range(0, len(yi)):
+            sum += yi[k]*fi[i].subs(x, xi[k])
+    return sum
+
+for i in range(0, len(fi)):
+    for j in range(0, len(fi)):
+        a[i][j] = prodInt(i, j, 'a')
+
+for i in range(0, len(fi)):
+    b[i] = prodInt(i, 0, 'b')
+
+print(a, "\n\n", b, "\n")
+
+sol = np.linalg.solve(a, b)
+
+print(sol)
